1a9585a1ab7840a40486b62eb891c42944299ec1
findhridoy <find.hridoy546@gmail.com>
2021-12-18 13:40:18 +0600

    added hikking and footer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const navMenu = document.getElementById("nav-menu");
 const navHamburger = document.getElementById("nav-hamburger");
 const navLink = document.querySelectorAll(".nav__link");
 
+// Toggle Hamburger Menu
 navHamburger.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
   navHamburger.classList.toggle("cross-hamburger");
@@ -12,4 +13,19 @@ navLink.forEach((item) => {
     navMenu.classList.remove("show-menu");
     navHamburger.classList.remove("cross-hamburger");
   });
+});
+
+// Hero Section parralax effect
+const topImage = document.getElementById("top-img");
+const middleImage = document.getElementById("middle-img");
+const bottomImage = document.getElementById("bottom-img");
+const heroData = document.getElementById("hero-data");
+
+window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY;
+  topImage.style.transform = `translateY(-${scrollPosition * 0.1}%)`;
+  middleImage.style.transform = `translateY(-${scrollPosition * 0.2}%)`;
+  bottomImage.style.transform = `translateY(-${scrollPosition * 0.1}%)`;
+  heroData.style.transform = `translateY(-${scrollPosition * 0.1}%)`;
+  heroData.style.opacity = 1 - scrollPosition * 0.004;
 });
